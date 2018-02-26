e1079a47cfce6a7d63dcdbe86a8b8db4e9474252
Ben Matthews <ben.matthews13@gmail.com>
2018-02-26 17:25:47 +0000

    save plots

--- script/smell-test-crime-type.R
+++ script/smell-test-crime-type.R
@@ -1,25 +1,25 @@
 
-# load packages -----------------------------------------------------------
+#' load packages -----------------------------------------------------------
 
 library(tidyverse)
 library(readxl)
-# read in data ------------------------------------------------------------
+#' read in data ------------------------------------------------------------
 
-# data from - http://www.gov.scot/Publications/2015/12/5123/downloads
+#' data from - http://www.gov.scot/Publications/2015/12/5123/downloads
 
 direct_receptions <- read_excel("data/raw/00491417.xlsx", sheet = "Table 11d", skip = 2) %>% 
   rename(crime_type = X__1)
 
-# removing NAs
+#' removing NAs
 direct_receptions <- 
   direct_receptions %>% 
   filter(!is.na(crime_type))
 
-# removing summary rows
+#' removing summary rows
 count(direct_receptions, crime_type) %>% View
 
 
-# selecting only crime groups
+#' selecting only crime groups
 crime_group <- 
 direct_receptions %>% 
   filter(crime_type == "Non-sexual crimes of violence" |
@@ -32,16 +32,16 @@ direct_receptions %>%
   gather("year", "n_direct_receptions", 2:11)
 
 
-# visualizing -------------------------------------------------------------
+#' visualizing -------------------------------------------------------------
 
-# plot by crime type
+#' plot by crime type
 crime_group %>% 
   ggplot(aes(x = year, y = n_direct_receptions, colour = crime_type, group = crime_type)) +
   geom_line() +
   facet_wrap(~ crime_type)
 
 
-# selecting only crimes
+#' selecting only crimes
 crime_type <- 
 direct_receptions %>% 
   filter(crime_type != "Total crimes & offences",
@@ -56,15 +56,15 @@ direct_receptions %>%
          crime_type != "Motor vehicle offences") %>% 
   gather("year", "n_direct_receptions", 2:11)
 
-# visualizing -------------------------------------------------------------
+#' visualizing -------------------------------------------------------------
 
-# plot by crime type
+#' plot by crime type
 crime_type %>% 
   ggplot(aes(x = year, y = n_direct_receptions, colour = crime_type, group = crime_type)) +
   geom_line() +
   facet_wrap(~ crime_type)
 
-# hmm, something weird is goin gon there with the other crime category
+#' hmm, something weird is going on there with the other crime category
 
 direct_receptions %>% 
   mutate(type_id = row_number()) %>% 
